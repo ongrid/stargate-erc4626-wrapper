98abc2a0434a65f2fdbe474ce4ec05bf2c15205f
Kirill Varlamov <kirill@ongrid.pro>
2024-05-17 18:02:53 +0200

    Add latest solc 0.8.25 as an additional compiler
    The base code contains 0.7.6 which is incompatible with oz5
    Added 0.8.25 so hardhat able to compile code inherited from modern oz

--- hardhat.config.js
+++ hardhat.config.js
@@ -71,13 +71,26 @@ function accounts(chainKey) {
  */
 module.exports = {
     solidity: {
-        version: "0.7.6",
-        settings: {
-            optimizer: {
-                enabled: true,
-                runs: 200,
+        compilers: [
+            {
+                version: "0.7.6",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                }
             },
-        },
+            {
+                version: "0.8.25",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                }
+            }
+        ]
     },
     contractSizer: {
         alphaSort: false,
