d71d4c9b7ac9575d9dead5684f073d81db14e9aa
Kirill Varlamov <kirill@ongrid.pro>
2024-05-20 01:45:03 +0200

    Enable logging in hardhat to debug solc 0.8 reverts

--- hardhat.config.js
+++ hardhat.config.js
@@ -107,6 +107,9 @@ module.exports = {
     defaultNetwork: "hardhat",
 
     networks: {
+        hardhat: {
+            loggingEnabled: true
+        },
         ethereum: {
             url: "https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161", // public infura endpoint
             chainId: 1,
